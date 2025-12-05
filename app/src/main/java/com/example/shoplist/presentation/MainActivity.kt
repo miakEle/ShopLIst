f85ab7b158a92ab2e103fd3ef73a53f0bcc48f72
miakele <myakushkinaea@gmail.com>
2025-12-05 08:40:31 +0200

    First domain, data, presentation

---
+++ app/src/main/java/com/example/shoplist/presentation/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.shoplist.presentation
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.shoplist.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.shopList.observe(this){
+            Log.d("MainActivityTest", it.toString())
+        }
+        viewModel.getShopList()
+
+    }
+}
